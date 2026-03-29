382486a388bd34561dd19559fec521653c807343
Iammoocle <iammoocle@gmail.com>
2026-03-28 18:14:45 -0700

    added api-key and hardcoded request

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from aibot!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+        raise Exception(f'Api key {api_key} is not valid')
+client = genai.Client(api_key=api_key)
+
+
+def main():
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."'
+    )
+    print(response.text)
+    
 
 if __name__ == "__main__":
     main()
