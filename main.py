50064788880776cbe070fc8c41892e103a54734c
Iammoocle <iammoocle@gmail.com>
2026-04-01 23:13:01 -0700

    added a verbose option

--- main.py
+++ main.py
@@ -15,13 +15,16 @@ client = genai.Client(api_key=api_key)
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
         model='gemini-2.5-flash', contents=messages
     )
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    if args.verbose == True:
+        print(f'User prompt: {args.user_prompt}')
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
     print('Response:')
     print(response.text)
     
