c880782dd9edd09e39226ef1a0a0c4c128f42b02
Iammoocle <iammoocle@gmail.com>
2026-04-05 23:00:32 -0700

    lernt 2 rite

---
+++ functions/write_file.py
@@ -0,0 +1,16 @@
+import os
+
+def write_file(working_directory,file_path,content):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(abs_working_dir, file_path))
+    valid_target_dir = os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
+    if not valid_target_dir:
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    
+    if not os.path.exists(os.path.dirname(target_dir)):
+            os.makedirs(os.path.dirname(target_dir))
+
+    with open(target_dir, "w") as f:
+        f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)}) characters written'
